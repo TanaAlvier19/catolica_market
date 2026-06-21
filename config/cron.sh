aa7ed51310e8699080348d4ca54e9afee517b674
Freddy491 <sacambinjafrederico@gmail.com>
2026-06-21 13:08:19 +0100

    Create cron.sh

---
+++ config/cron.sh
@@ -0,0 +1,18 @@
+arquivo="backup.sh"
+
+#Função que programa a execução automática de um script
+confCron()
+{
+    cronConf="0 18 * * * $(pwd)/$1"
+    echo "adicionando: $cronConf "
+    (crontab -l 2>/dev/null | grep -Fv "$cronConf"; echo "$cronConf") | crontab -
+    echo "Código de retorno: $?"
+}
+
+# definir execução automática para o backup
+confCron "$arquivo"
+
+# definir execução automática para o manutencao
+arquivo="manutencao.sh"
+confCron "$arquivo"
+exit 1
