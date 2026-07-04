24649aec1f1af00bee8c5ccd301d55488801240d
Freddy491 <sacambinjafrederico@gmail.com>
2026-07-04 19:57:44 +0100

    Update gestao_clientes.sh

--- bin/gestao_clientes.sh
+++ bin/gestao_clientes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ARQUIVO_CLIENTES="/opt/catolica_market/data/clientes/clientes.txt"
+ARQUIVO_CLIENTES="../data/clientes/clientes.txt"
 source /opt/catolica_market/lib/funcoes.sh
 
 # Estrutura do registo: numero_cartao;nome;nif;saldo;data_cadastro
